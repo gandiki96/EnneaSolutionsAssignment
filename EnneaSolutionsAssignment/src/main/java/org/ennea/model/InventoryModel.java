1a24f336aa6731e7eaf99c8fbc527405f3725f11
gandiki96 <106311678+gandiki96@users.noreply.github.com>
2022-05-30 10:33:09 +0530

    Update InventoryModel.java
    Added default values to handle invalid data from db

--- EnneaSolutionsAssignment/src/main/java/org/ennea/model/InventoryModel.java
+++ EnneaSolutionsAssignment/src/main/java/org/ennea/model/InventoryModel.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -15,47 +16,42 @@ import lombok.Data;
 @Table(name = "inventory",schema = "enneadev")
 public class InventoryModel {
 	
-	@Id
-	@Column(name = "id")	
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)    
 	private Long id;
 	
 	@Column(name = "product_code")	
-	private String productCode;
+	private String productCode = "N/A";
 	
 	@Column(name = "product_name")
-	private String productName;
+	private String productName = "N/A";
 	
 	@Column(name = "stock")
-	private Integer stock;
+	private Integer stock = 0;
 	
 	@Column(name = "deal")
-	private Integer deal;
+	private Integer deal = 0;
 	
 	@Column(name = "free_count")
-	private Integer free;
+	private Integer free = 0;
 	
 	@Column(name = "mrp")
-	private Double mrp;
+	private Double mrp = 0.0;
 	
 	@Column(name = "rate")
-	private Double rate;
+	private Double rate = 0.0;
 	
-	@Column(name = "expiry_date")
-	@DateTimeFormat(pattern = "dd-mm-yyyy")
-	private String date;
+	@Column(name = "expiry_date")	
+	private String date = "/ /";
 	
 	@Column(name = "batch")
-	private String batch;
+	private String batch = "N/A";
 	
 	@Column(name = "company")
-	private String company;
+	private String company = "N/A";
 	
 	@Column(name = "supplier")
-	private String supplier;
-	
-	
-	
-	
+	private String supplier = "N/A";	
 
 }
